6b1c2ac2bf8b06453a51a4e4603ce05c78b024cc
sebastianEspino <sebasespinosa2016@gmail.com>
2026-02-25 18:59:02 -0500

    first commit

---
+++ src/main/java/com/example/SaaSMultiTentBackEnd/SaaSMultiTentBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.SaaSMultiTentBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaSMultiTentBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaaSMultiTentBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SaaSMultiTentBackEnd/SaaSMultiTentBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SaaSMultiTentBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaaSMultiTentBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
